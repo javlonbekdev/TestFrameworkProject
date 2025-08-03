7602d9ebb8314abbea69468c1796e601c5518415
javlonbekdev <javlonbekdev@gmail.com>
2025-08-03 12:07:09 +0500

    Initial Commit

---
+++ TestFrameworkProject/TestFrameworkProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestFrameworkProjectApp.swift
+//  TestFrameworkProject
+//
+//  Created by Javlonbek Dev on 03/08/25.
+//
+
+import SwiftUI
+
+@main
+struct TestFrameworkProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
